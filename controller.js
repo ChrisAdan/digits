a76f3ebdf18301de784440297362baef1f034fec
Chris Adan <55331613+ChrisAdan@users.noreply.github.com>
2024-08-11 15:06:07 -0400

    Map out functions

---
+++ controller.js
@@ -0,0 +1,34 @@
+"use strict";
+// imports
+
+// setup
+
+// add
+
+// subtract
+
+// multiply
+
+// divide
+
+// invertSign
+
+// sqrt
+
+// cubeRoot
+
+// raiseToPower
+
+// modulus
+
+// percentage
+
+// undo
+
+// clearAll
+
+// powerOff
+
+// operate
+
+// renderUpdate
---
+++ model.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// add
+
+// subtract
+
+// multiply
+
+// divide
+
+// invertSign
+
+// sqrt
+
+// cubeRoot
+
+// raiseToPower
+
+// modulus
+
+// percentage
+
+// undo
+
+// clearAll
+
+// powerOff
+
+// operate
+
+// validate
+
+// divideByZero
+
+// exports
